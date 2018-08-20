fa72ed9b777489879438713148c55ab3607e3f35
Meikel Philipp <meikel.philipp@neos-it.de>
2018-08-20 15:21:37 +0200

    [ADD] Ability to change themes [ADD] Add dark theme [CHG] Bulma now comes from CDN [CHG] Some minor changes

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,5 @@
+
+
 // Get all "navbar-burger" elements
 var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
@@ -5,8 +7,8 @@ var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navb
 if ($navbarBurgers.length > 0) {
 
   // Add a click event on each of them
-  $navbarBurgers.forEach(function($el) {
-    $el.addEventListener('click', function() {
+  $navbarBurgers.forEach(function ($el) {
+    $el.addEventListener('click', function () {
 
       // Get the target from the "data-target" attribute
       var target = $el.dataset.target;
@@ -18,4 +20,46 @@ if ($navbarBurgers.length > 0) {
 
     });
   });
+}
+
+/////
+// Themes controlled by JS
+/////
+
+const themes = {
+  dark: {
+    theme: "https://cdnjs.cloudflare.com/ajax/libs/bulmaswatch/0.7.1+1/solar/bulmaswatch.min.css",
+    highlight: "/kotlin-for-beginners/assets/highlight-dark.css"
+  },
+  light: {
+    theme: "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css",
+    highlight: "/kotlin-for-beginners/assets/highlight-light.css"
+  }
+}
+
+const themeSwticher = document.getElementById('theme-switcher')
+themeSwticher.checked = localStorage.getItem('theme') === 'dark'
+themeSwticher.addEventListener('change', _ => {
+  refreshTheme(themeSwticher.checked)
+  const theme = getThemeKey(themeSwticher.checked)
+  localStorage.setItem('theme', theme)
+  console.log(themeSwticher.checked)
+})
+
+function refreshTheme(toggle) {
+  const newTheme = getTheme(toggle)
+  document.getElementById('theme').href = newTheme.theme
+  document.getElementById('theme-highlight').href = newTheme.highlight
+}
+
+function getTheme(toggle) {
+  return themes[getThemeKey(toggle)]
+}
+
+function getThemeKey(toggle) {
+  if (toggle) {
+    return 'dark'
+  } else {
+    return 'light'
+  }
 }
